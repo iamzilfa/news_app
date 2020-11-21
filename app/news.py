eb3f42c7f2a76ffe7d1791080691c30b5e9f3149
Zilfa Cyamani <zilcyam@gmail.com>
2020-11-21 23:47:18 +0200

    editing news file

--- app/news.py
+++ app/news.py
@@ -1,21 +1,12 @@
-class News_Sources:
+class News:
 
-    def __init__(self,id,name,url,category,description):
-        self.news_id = id
-        self.news_name = name
-        self.news_url = url
-        self.news_category = category
-        self.news_description = description
+    def __init__ (self,urlToImage,title,description,author,publishedAt,url):
 
-
-class News_Article:
-
-    def __init__(self,id,author,title,description,publishedAt,urlToImage,url,):
-
-        self.new_id = id
-        self.news_author = author
+        self.news_urlToImage = urlToImage
         self.news_title = title
         self.news_description = description
+        self.news_author = author
         self.news_publishedAt = publishedAt
-        self.news_urlToImage = urlToImage
         self.news_url = url
+        
+        
