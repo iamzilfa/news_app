9a4246c40646a025578311383ddc965488478713
Zilfa Cyamani <zilcyam@gmail.com>
2020-11-21 11:11:58 +0200

    add for loop in index.html

--- app/views.py
+++ app/views.py
@@ -10,4 +10,5 @@ def index():
     View root page function that returns the index page and its data
     '''
     myNewsdata = my_news()
-    return render_template('index.html',)
+    print(myNewsdata)
+    return render_template('index.html', data = myNewsdata)
