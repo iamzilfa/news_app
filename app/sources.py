1139811a3d317b5083f71d71b298e736cd4f1098
Zilfa Cyamani <zilcyam@gmail.com>
2020-11-21 23:47:47 +0200

    sources file

---
+++ app/sources.py
@@ -0,0 +1,9 @@
+class Source:
+
+    def __init__ (self,category,name,description,url):
+
+        self.news_category = category
+        self.news_name = name
+        self.news_description = description
+        self.news_url = url
+        
