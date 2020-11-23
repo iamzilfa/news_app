799425ad475e1df23986729e718587ebdd8e0503
Zilfa Cyamani <zilcyam@gmail.com>
2020-11-23 21:59:08 +0200

    Deploying to heroku

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+import os
 class Config:
     '''
     General configuration parent class
@@ -5,6 +6,7 @@ class Config:
 
     SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?category={}&apiKey={}'                  
     ARTICLE_API_NEWS_URL ='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     
 
 
